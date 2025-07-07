4b6db7799758a52906bddda7683af098b46226da
Jacob Møhl <hello@jacobmohl.dk>
2025-07-07 20:13:17 +0200

    Update Functions

--- server/src/BeachWeatherStation.Worker/Program.cs
+++ server/src/BeachWeatherStation.Worker/Program.cs
@@ -1,14 +1,34 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-var builder = FunctionsApplication.CreateBuilder(args);
-
-builder.ConfigureFunctionsWebApplication();
-
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults(builder => {}, options =>
+    {
+        //options.EnableUserCodeException = true;
+    })    
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddMvc(); // Add MVC services for handling HTTP requests;
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });     
+           
+        // Disable IHttpClientFactory Informational logs.
+        // Note -- you can also remove the handler that does the logging: https://github.com/aspnet/HttpClientFactory/issues/196#issuecomment-432755765 
+        //logging.AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
+    })    
+    .Build();
+host.Run();
